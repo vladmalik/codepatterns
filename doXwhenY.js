77b3249066886f0d2c6f38f513549fa28189be82
vladmalik <vladmalik@gmail.com>
2015-08-15 11:15:20 -0400

    Update doXwhenY.js

--- doXwhenY.js
+++ doXwhenY.js
@@ -1,6 +1,6 @@
 // Supports multiple targets
 
-var targetY = []; // List targets in array
+var targetY = []; // List targets in array e.g., ["#my-element"]
 var isActivated = []; // Leave blank
 var scrollY;
 var windowHeight = $(window).height();
@@ -8,14 +8,15 @@ var targetSensitivity = 300; // Target has to scroll 300px into view
 var scrollSensitivity = 200; // Check every 200 ms
 
 $(window).load(function () {
-	targetY[0] = Math.ceil($(".selector").offset().top); // Get rendered position of first target
+	targetY[0] = Math.ceil($(targetY[0]).offset().top); // Get rendered position of first target
 });	
 
 var atTargetY = setInterval(function() {
 	scrollY = $(document).scrollTop(); // refresh scroll position
-
+	windowHeight = $(window).height();
+	
 	// Listen for first target (repeat this block with target[1]... for other targets)
-	if(targetY[0]!=0 && scrollY + windowHeight-targetSensitivity > 	targetY[0]) {
+	if(scrollY + windowHeight-targetSensitivity > 	targetY[0] && isActivated[0]!=1) {
 	  // doStuff();
 	  isActivated[0]=1;
 	}
